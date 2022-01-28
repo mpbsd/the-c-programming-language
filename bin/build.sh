88655ae24e58ddaf635d66e9fae0a6adb4816454
Marcelo Barboza <salve.barboza@pm.me>
2022-01-28 10:57:25 -0300

    a simple shell script to compile the exercises
      and place them in the bin folder for us

--- bin/build.sh
+++ bin/build.sh
@@ -17,17 +17,79 @@ function compile() {
 
 
 function clean() {
-  if [ -z "$(ls ${ROOT}/bin | egrep '[A-Z0-9]{5}\.o')" ]
+  if [[ -n "$(ls ${ROOT}/bin | egrep '[A-Z0-9]{5}\.o')" ]]
   then
-    printf "${ROOT}/bin is already clean\n"
-  else
     rm ${ROOT}/bin/*.o
     printf "${ROOT}/bin has been cleared\n"
+  else
+    printf "${ROOT}/bin is already clean\n"
   fi
 }
 
+
 function help() {
-  printf "print a help message\n"
+  printf "Basic usage:"
+  printf "\n\t./bin/build.sh [-c, --clean]                               # Remove bin/*.o files"
+  printf "\n\t./bin/build.sh [-h, --help]                                # Print this help message"
+  printf "\n\t./bin/build.sh [-chp, --chapter] 2 [-exr, --exercise] 03   # Compile exercise 03 from chapter 2\n"
 }
+
+
+function error_message() {
+  printf "Error: unrecognized options\n"
+  printf "Type in ./bin/build.sh [-h, --help] for help\n"
+}
+
+
+if [[ $# -eq 1 ]]
+then
+  case $1 in
+    '-c' | '--clean')
+      clean
+      ;;
+    '-h' | '--help')
+      help
+      ;;
+    *)
+      error_message
+      ;;
+  esac
+elif [[ $# -eq 4 ]]
+then
+  case $1 in
+    '-chp' | '--chapter')
+      CHP=$2
+      case $3 in
+        '-exr' | '--exercise')
+          EXR=$4
+          ;;
+      esac
+      ;;
+    '-exr'| '--exercise')
+      EXR=$2
+      case $3 in
+        '-chp' | '--chapter')
+          CHP=$4
+          ;;
+      esac
+      ;;
+    *)
+      error_message
+      ;;
+  esac
+  if [[ -n $CHP ]] && [[ -n $EXR ]]
+  then
+    SRC="${ROOT}/chp/${CHP}/exerc/${EXR}.c"
+    if [[ -f $SRC ]]
+    then
+      compile $CHP $EXR
+    else
+      printf "Error: file\n\n\t$SRC\n\ndoesn't exist\n"
+    fi
+  else
+    error_message
+  fi
+fi
+
 
 exit 0
